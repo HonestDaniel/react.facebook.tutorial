cbd41020a9904ce147cb6e23c70a8fdb7966ecc5
Alexander Kochemasov <akochemasov@gmail.com>
2019-04-26 19:08:34 +0400

    Перенес состояние игры в компонент Game

--- src/index.js
+++ src/index.js
@@ -13,65 +13,21 @@ function Square(props) {
 
 // Board
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-      winner: null
-    }
-  }
-
-  handleClick = (i) => {
-    const squares = this.state.squares.slice();   //для создания копии массива, вместо изменения существующего массива    
-    if (this.state.winner || squares[i]) {
-      return;
-    }
-    
-    squares[i] = this.state.xIsNext ? 'X' : '0';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-      winner: this.calculateWinner(squares)
-    })
-  }
-
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]} 
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
 
-  calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    
-    return lines.reduce((acc, item) => {
-      const [a, b, c] = item;
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-      return acc;
-    }, null)
-  }
-
   render() {
     let status = '';
-    if (this.state.winner) {
-      status = `Выиграл ${this.state.winner}`;
+    if (this.props.winner) {
+      status = `Выиграл ${this.props.winner}`;
     } else {
-      status = `Следующий ход: ${this.state.xIsNext ? 'X' : '0'}`;
+      status = `Следующий ход: ${this.props.xIsNext ? 'X' : '0'}`;
     }
     
     return(
@@ -99,14 +55,63 @@ class Board extends React.Component {
 
 // Game
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: Array(9).fill(null),
+      xIsNext: true,
+      winner: null
+    }
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    return lines.reduce((acc, item) => {
+      const [a, b, c] = item;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+      return acc;
+    }, null)
+  }
+
+  onHandleClick = (i) => {
+    const squares = this.state.history.slice();   //для создания копии массива, вместо изменения существующего массива    
+    if (this.state.winner || squares[i]) {
+      return;
+    }
+    
+    squares[i] = this.state.xIsNext ? 'X' : '0';
+    this.setState({
+      history: squares,
+      xIsNext: !this.state.xIsNext,
+      winner: this.calculateWinner(squares)
+    })
+  }
+
   render() {
     return(
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={this.state.history}
+            onClick={(i) => this.onHandleClick(i)}
+            winner={this.state.winner}
+            xIsNext={this.state.xIsNext}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{/* status */}</div>          
         </div>
       </div>
     )
